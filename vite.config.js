1397c5fbb576fd08f2b8de0054f6adcfe49fa605
Babajide21 <alarapebabajide45@gmail.com>
2025-06-05 13:43:13 +0100

    changes by kenn

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,25 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import { resolve } from "path";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
+// ESM __dirname workaround
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   build: {
-rollupOptions: {
-  input: {
-    main: resolve(__dirname,'index.html'),
-    about: resolve(__dirname,'about.html'),
-    blog: resolve(__dirname,'blog.html'),
-    contact: resolve(__dirname,'contact.html'),
-    home: resolve(__dirname,'home.html'),
-  }
-}
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        about: path.resolve(__dirname, 'about.html'),
+        blog: path.resolve(__dirname, 'blog.html'),
+        contact: path.resolve(__dirname, 'contact.html'),
+        home: path.resolve(__dirname, 'home.html'),
+      }
+    }
   },
   plugins: [
     tailwindcss(),
   ],
-})
+});
