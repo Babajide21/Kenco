e10bcafb906e4d258de21b08b68875dc22a253ec
Babajide21 <alarapebabajide45@gmail.com>
2025-06-05 13:19:09 +0100

    Changes by baba jide

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  build: {
+rollupOptions: {
+  input: {
+    main: resolve(__dirname,'index.html'),
+    about: resolve(__dirname,'about.html'),
+    blog: resolve(__dirname,'blog.html'),
+    contact: resolve(__dirname,'contact.html'),
+    home: resolve(__dirname,'home.html'),
+  }
+}
+  },
   plugins: [
     tailwindcss(),
   ],
